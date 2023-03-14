722db7515f59596cda9b56e6f1f864e9f2774f77
Akhmadjon <ahmadjonzuhriddinov84@gmail.com>
2023-03-15 00:37:07 +0900

    wonk on templates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Akhmadjon',
+        'title': 'blog post 1',
+        'content': 'first post content',
+        'date_posted': 'march 15, 2023'
+    },
+    {
+        'author': 'Aki',
+        'title': 'blog post 2',
+        'content': 'second post content',
+        'date_posted': 'march 16, 2023'
+    },
+
+]
 
 
 def home(request):
-    return HttpResponse('Blog home')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
